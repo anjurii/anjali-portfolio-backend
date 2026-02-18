8bc77768552294d45aebb184f3b2e0216df84345
InfernoRider10 <sarawadeh@gmail.com>
2026-02-18 15:13:20 +0530

    Fix PORT for Render deployment

--- server.js
+++ server.js
@@ -15,7 +15,8 @@ mongoose.connect(process.env.MONGO_URI)
 const contactRoutes = require('./routes/contactRoutes');
 app.use('/api', contactRoutes);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
